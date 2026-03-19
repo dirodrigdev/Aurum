0b8996f606a1a1ca0a70b3447b51db35aeaa95aa
dirodrigdev <diegorp.1978@gmail.com>
2026-03-19 03:49:02 +0100

    commit

---
+++ apps/midas/src/types/recharts.d.ts
@@ -0,0 +1,8 @@
+declare module 'recharts' {
+  export const AreaChart: any; export const Area: any;
+  export const LineChart: any; export const Line: any;
+  export const ScatterChart: any; export const Scatter: any;
+  export const BarChart: any; export const Bar: any;
+  export const XAxis: any; export const YAxis: any; export const CartesianGrid: any; export const Tooltip: any;
+  export const ResponsiveContainer: any; export const ReferenceLine: any; export const Cell: any;
+}
